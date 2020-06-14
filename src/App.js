f0ad76cbdd86ea55992d7f16cb1b93c2ce57c8df
Vuong Hong An <anvh87@gmail.com>
2020-06-14 23:31:36 +0700

    Add Search field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Row, Col, Label, Input } from 'reactstrap'
+import { Container, Row, Col, Label, Input, Button } from 'reactstrap'
 import { ToastContainer } from 'react-toastify';
 import ModalForm from './components/Modals/Modal'
 import PatientsTable from './components/Tables/PatientsTable'
@@ -7,11 +7,20 @@ import PatientsTable from './components/Tables/PatientsTable'
 class App extends Component {
   state = {
     patients: [],
-    showSoftDeleted: false
+    showSoftDeleted: false,
+    searchField: 'FirstName',
+    sortDirection: null,
+    searchText: null,
   }
 
   getPatients = () => {
-    const url = this.state.showSoftDeleted ? 'http://localhost:5000/patients?withDeleted=true' : 'http://localhost:5000/patients'
+    let url = this.state.showSoftDeleted ? 'http://localhost:5000/patients?withDeleted=true' : 'http://localhost:5000/patients'
+    if (this.state.searchField && this.state.searchText) {
+      url = `${url}${url.includes('?') ? '&' : '?'}${this.state.searchField}=${this.state.searchText}`
+    }
+    if (this.state.sortDirection) {
+      url = `${url}${url.includes('?') ? '&' : '?'}sort=${this.state.sortDirection}`
+    }
     fetch(url)
       .then(response => response.json())
       .then(patients => this.setState({patients}))
@@ -36,6 +45,18 @@ class App extends Component {
     setTimeout(() => this.getPatients(), 0)
   }
 
+  onSearchFieldChanged = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  onSortDirectionChanged = e => {
+    this.setState({ sortDirection: e.target.value })
+  }
+
+  onSearchTextChanged = e => {
+    this.setState({ searchText: e.target.value })
+  }
+
   componentDidMount(){
     this.getPatients()
   }
@@ -46,6 +67,29 @@ class App extends Component {
         <Row>
           <Col>
             <h1 style={{margin: "20px 0"}}>Patients List</h1>
+          </Col>
+        </Row>
+        <Row style={{margin: "20px 0"}}>
+          <Col xs="2">
+            <Input type="select" value={this.state.searchField === null ? 'FirstName' : this.state.searchField} onChange={this.onSearchFieldChanged}>
+              <option value="FirstName">First Name</option>
+              <option value="LastName">Last Name</option>
+              <option value="PatientID">PatientID</option>
+              <option value="gender">Gender</option>
+              <option value="dob">Date of Birth</option>
+            </Input>
+          </Col>
+          <Col xs="2.5">
+            <Input type="select" value={this.state.sortDirection === null ? 'desc' : this.state.sortDirection} onChange={this.onSortDirectionChanged}>
+              <option value="desc">Descending Sort</option>
+              <option value="asc">Ascending Sort</option>
+            </Input>
+          </Col>
+          <Col xs="5">
+            <Input type="text" onChange={this.onSearchTextChanged} placeholder='Search content'/>
+          </Col>
+          <Col xs="2">
+            <Button color="primary" onClick={this.getPatients}>Search</Button>
           </Col>
         </Row>
         <Row>
